91b8865a51ea755e08026ba91f0920c15ef3f8bc
yonghwan <coxldtpeo1@naver.com>
2023-09-19 18:27:56 +0900

    랜딩페이지 pc 1차 작업 완료

---
+++ js/index.js
@@ -0,0 +1,41 @@
+history.scrollRestoration = "manual";
+const wrap = document.getElementsByClassName("wrap")[0]; // 보일 영역
+const slide = document.getElementsByClassName("slide");
+let page = 0; // 영역 포지션 초기값
+const lastPage = slide.length - 1; // 마지막 페이지
+
+const slideLength = document.querySelectorAll(".slide").length;
+let createBullet = ``;
+for (i = 0; i < slideLength; i++) {
+  createBullet += `<li class="bullet" data-index=${i}>○</li>`;
+}
+document.querySelector(".pagination ul").innerHTML = createBullet;
+
+window.addEventListener(
+  "wheel",
+  e => {
+    e.preventDefault();
+    if (e.deltaY > 0) {
+      page++;
+    } else if (e.deltaY < 0) {
+      page--;
+    }
+    if (page < 0) {
+      page = 0;
+    } else if (page > lastPage) {
+      page = lastPage;
+    }
+    console.log(e.deltaY);
+    wrap.style.top = page * -100 + "vh";
+    console.log(page);
+  },
+  { passive: false }
+); // 디폴트 기능 제거 - 스크롤
+
+document.querySelectorAll(".bullet").forEach(el => {
+  el.addEventListener("click", e => {
+    const index = e.target.dataset.index;
+    page = Number(index);
+    wrap.style.top = page * -100 + "vh";
+  });
+});
