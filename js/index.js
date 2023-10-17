0378552a7876f26d29094c79b0ec329b77ba099a
yonghwan <coxldtpeo1@naver.com>
2023-10-17 18:26:41 +0900

    랜딩페이지pc 수정사항 반영

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-history.scrollRestoration = "manual";
-const wrap = document.getElementsByClassName("wrap")[0]; // 보일 영역
-const slide = document.getElementsByClassName("slide");
-let page = 0; // 영역 포지션 초기값
-let animation = true;
-const lastPage = slide.length - 1; // 마지막 페이지
-const slideLength = document.querySelectorAll(".slide").length;
-let createBullet = ``;
-for (i = 0; i < slideLength; i++) {
-  createBullet += `<li class="bullet" data-index=${i}></li>`;
-}
-document.querySelector(".pagination ul").innerHTML = createBullet;
+// history.scrollRestoration = "manual";
+// const wrap = document.getElementsByClassName("wrap")[0]; // 보일 영역
+// const slide = document.getElementsByClassName("slide");
+// let page = 0; // 영역 포지션 초기값
+// let animation = true;
+// const lastPage = slide.length - 1; // 마지막 페이지
+// const slideLength = document.querySelectorAll(".slide").length;
+// let createBullet = ``;
+// for (i = 0; i < slideLength; i++) {
+//   createBullet += `<li class="bullet" data-index=${i}></li>`;
+// }
+// document.querySelector(".pagination ul").innerHTML = createBullet;
 
 // window.addEventListener(
 //   "wheel",
@@ -35,11 +35,11 @@ document.querySelector(".pagination ul").innerHTML = createBullet;
 //   // { passive: false }
 // ); // 디폴트 기능 제거 - 스크롤
 
-document.querySelectorAll(".bullet").forEach(el => {
-  el.addEventListener("click", e => {
-    const index = e.target.dataset.index;
-    page = Number(index);
-    // wrap.style.top = page * -100 + "vh";
-    scrollTo({ left: 0, top: page * window.innerHeight, behavior: "smooth" });
-  });
-});
+// document.querySelectorAll(".bullet").forEach(el => {
+//   el.addEventListener("click", e => {
+//     const index = e.target.dataset.index;
+//     page = Number(index);
+//     // wrap.style.top = page * -100 + "vh";
+//     scrollTo({ left: 0, top: page * window.innerHeight, behavior: "smooth" });
+//   });
+// });
